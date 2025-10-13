60c6ba5920e4ac011084b16bd643859694da7b66
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-10-13 16:31:11 -0400

    Design ContactRepository to interact with the database.

---
+++ src/main/java/org/eimc/contactcardapplication/repository/ContactRepository.java
@@ -0,0 +1,12 @@
+package org.eimc.contactcardapplication.repository;
+
+import org.eimc.contactcardapplication.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    Optional<Contact> findById(Long id);            // May or may not contain a Contact object.
+}
