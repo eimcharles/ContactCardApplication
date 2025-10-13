45f98410abc7501d47491b460c4317e5dc5b63fb
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-10-13 16:36:37 -0400

    Added Comment for class context

--- src/main/java/org/eimc/contactcardapplication/repository/ContactRepository.java
+++ src/main/java/org/eimc/contactcardapplication/repository/ContactRepository.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/*
+ *       Contact Repository
+ * */
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     Optional<Contact> findById(Long id);            // May or may not contain a Contact object.
