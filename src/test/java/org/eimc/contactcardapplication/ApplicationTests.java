b62a135e5dab5beeedeadbaab6f82e83047356a1
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-10-13 15:22:34 -0400

    Initial Commit for Backend Structure

---
+++ src/test/java/org/eimc/contactcardapplication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eimc.contactcardapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
