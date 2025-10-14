e714535c41024807d3920f28a6857e8dfa41dab0
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-10-14 14:24:28 -0400

    Adding ContactRepository dependency and parameterized constructor

--- src/main/java/org/eimc/contactcardapplication/service/ContactService.java
+++ src/main/java/org/eimc/contactcardapplication/service/ContactService.java
@@ -1,10 +1,22 @@
 package org.eimc.contactcardapplication.service;
 
 import jakarta.transaction.Transactional;
+import org.eimc.contactcardapplication.repository.ContactRepository;
 import org.springframework.stereotype.Service;
+
+
+/*
+*   Service Class (Business Layer)
+*
+* */
 
 @Service
 @Transactional(rollbackOn = Exception.class)            // Rolls back transactions only for unchecked exceptions
 public class ContactService {
 
+    private final ContactRepository contactRepository;
+
+    public ContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
 }
